89aee1b6d58ec45fcd8c933e3c0aee01ea22e7b7
Apaar gupta <apaar.gupta.13@gmail.com>
2017-04-20 14:17:13 +0530

    comment indentation

--- ncbi-blast/split_output.sh
+++ ncbi-blast/split_output.sh
@@ -1,14 +1,14 @@
 
 #For first few line
-head -15l 500k_600k_ParallelOutput0.txt > FinalOutput.txt    			#copies first 15 lines of partition output to final output
-QueryDetail=$(head -1l 8Million_DNA.fa)									#first line, description of fasta file
-QueryDetail=${QueryDetail#">"}											#add '>' symbol to description
-sed '1d' 8Million_DNA.fa > temp_DNA.txt									#deletes first line and copies only the sequence into another file
+head -15l 500k_600k_ParallelOutput0.txt > FinalOutput.txt    	#copies first 15 lines of partition output to final output
+QueryDetail=$(head -1l 8Million_DNA.fa)		#first line, description of fasta file
+QueryDetail=${QueryDetail#">"}			#add '>' symbol to description
+sed '1d' 8Million_DNA.fa > temp_DNA.txt		#deletes first line and copies only the sequence into another file
 count_DNA=$(wc -c < temp_DNA.txt)										
-count_DNA=$(($count_DNA-2))												#character count of dna sequence			
+count_DNA=$(($count_DNA-2))			#character count of dna sequence			
 QueryDetail="Query = "$QueryDetail										
 QueryDetail=$QueryDetail"\n\nLength = "$count_DNA
-echo $QueryDetail >> FinalOutput.txt									#appends Query Detail to end of final output file
+echo $QueryDetail >> FinalOutput.txt		#appends Query Detail to end of final output file
 blah="\t\t\t\t\t\t\t\t\t\t\t\t\tScore\t\tE\nSequences producing significant alignments:\t\t\t(Bits)\tValue"
 subjectDesc=$(head -1l prot_sample1.fa)
 subjectDesc=${subjectDesc#'>'}
@@ -22,12 +22,12 @@ for i in $(seq 0 7);do
 	head -n -22 500k_600k_ParallelOutput$i.txt > temp.txt 
 	mv  temp.txt 500k_600k_ParallelOutput$i.txt	
 	csplit --quiet --prefix=loop$i --digits=1  500k_600k_ParallelOutput$i.txt /Score/ {*}
-	sed '1d' DNA_split_sequence0$i.fa > temp_DNA.txt					#deletes first line and copies only the sequence into another file
+	sed '1d' DNA_split_sequence0$i.fa > temp_DNA.txt	#deletes first line and copies only the sequence into another file
 	count_partition=$(wc -c < temp_DNA.txt)
 	ratio=$(echo "scale=4; $count_DNA / $count_partition"|bc)
 	rm loop"$i"0
 	for f in loop$i*;do
-		e=$(grep -Po 'Expect = \K.*(?=,)'  ${f})						#computing the new E value
+		e=$(grep -Po 'Expect = \K.*(?=,)'  ${f})		#computing the new E value
 		new_e=$(echo "scale=4;($ratio*$e)/1"|bc)
 		
 		t_e=10.0
